f10b73bb76f54132fbb4f64c31afbe589b1e37ec
alexquar <alexanderquarrie@gmail.com>
2024-03-26 13:25:45 -0400

    fetching

---
+++ src/components/TripList.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+export default function TripList() {
+    const [trips,setTrips] = useState([])
+
+fetch('http://localhost:3000/trips')
+.then( response => response.json())
+.then( json => setTrips(json))
+.catch(err=>console.log(err))
+  return (
+    <div>
+      <h2> Trip List</h2>
+    </div>
+  )
+}
