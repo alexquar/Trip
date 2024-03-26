7c733f46a8c14c70d998d108cdf13a0f3d8c88df
alexquar <alexanderquarrie@gmail.com>
2024-03-26 13:41:32 -0400

    useEffect

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react"
-
+import { useState, useEffect } from "react"
+import './TripList.css'
 export default function TripList() {
     const [trips,setTrips] = useState([])
+    console.log(trips)
+    useEffect(()=>{
+        fetch('http://localhost:3000/trips')
+        .then( response => response.json())
+        .then( json => setTrips(json))
+        .catch(err=>console.log(err)) 
+    }, [
+        //dependency array, to make the function run only once leave empty
+    ])
 
-fetch('http://localhost:3000/trips')
-.then( response => response.json())
-.then( json => setTrips(json))
-.catch(err=>console.log(err))
-  return (
-    <div>
-      <h2> Trip List</h2>
-    </div>
-  )
+
+    return (
+        <div className="trip-list">
+          <h2>Trip List</h2>
+          <ul>
+            {trips.map(trip => (
+              <li key={trip.id}>
+                <h3>{trip.title}</h3>
+                <p>{trip.price}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
 }
